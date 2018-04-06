e68a7885f404a5ed8731bf9192b98ff72e266514
Esteban D. Barboza Primera <dario0117@gmail.com>
2018-04-05 20:24:34 -0500

    Agregada la clase board - Las instancias de esta clase se construyen dependiendo del tamaño (size) de cada número y de la cantidad, esta clase lo que hace es construir una matriz en la que se van a imprimir todas las demás matrices, una al lado de la otra, estas matrices corresponden a cada dígito ingresado por el usuario

---
+++ src/Board.js
@@ -0,0 +1,32 @@
+const Digit = require('./Digit');
+const Utils = require('./Utils');
+
+class Board{
+    constructor({size, numbers}){
+        this.numbers = numbers;
+        this.lcd = new Digit(size);
+        let col = (this.lcd.columns * this.numbers.length) + (this.numbers.length - 1);
+        this.board = Utils.matrix(this.lcd.rows, col);
+        this.spaces_between_numbers = 1;
+    }
+
+    generate(){
+        let actual_column = 0;
+        for(let i = 0; i < this.numbers.length; i++){
+            let tmp_number = this.lcd.generate(this.numbers[i]);
+            for(let row = 0; row < this.lcd.rows ; row++){
+                for(let col = 0; col < tmp_number[row].length; col++){
+                    this.board[row][col + actual_column] = tmp_number[row][col];
+                }
+            }
+            actual_column += tmp_number[0].length + this.spaces_between_numbers;
+        }
+    }
+
+    print(){
+        this.generate();
+        Utils.printMatrix(this.board);
+    }
+}
+
+module.exports = Board;
