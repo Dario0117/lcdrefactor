8f0cc9110a16f303e41a5b9f142f7c4bbfd6fa61
Esteban D. Barboza Primera <dario0117@gmail.com>
2018-04-06 20:36:24 -0500

    Refactorizado el mecanismo para generar cada dígito

--- src/Digit.js
+++ src/Digit.js
@@ -2,7 +2,6 @@ const Utils = require('./Utils');
 
 class Digit{
     constructor(size){
-        this.size = size;
         this.rows = 2 * size + 3;
         this.columns = 2 + size;
         this.horizontal = '-';
@@ -10,11 +9,20 @@ class Digit{
     }
 
     generate(number){
+        let matrix = Utils.matrix(this.rows, this.columns);
         switch(number){
             case 0:
-                return this.zero();
+                this.addTop(matrix);
+                this.addBot(matrix);
+                this.addLeftTop(matrix);
+                this.addLeftBot(matrix);
+                this.addRightTop(matrix);
+                this.addRightBot(matrix);
+                return matrix;
             case 1:
-                return this.one();
+                this.addRightTop(matrix);
+                this.addRightBot(matrix);
+                return matrix;
             case 2:
                 break;
             case 3:
@@ -34,28 +42,40 @@ class Digit{
         }
     }
 
-    zero(){
-        let matrix = Utils.matrix(this.rows, this.columns);
+    addTop(matrix){
         for(let column = 1 ; column < this.columns - 1 ; column++){
             matrix[0][column] = this.horizontal;
-            matrix[this.rows - 1][column] = this.horizontal;
         }
-        for(let row = 1 ; row < this.rows - 1; row++){
-            matrix[row][0] = this.vertical;
-            matrix[row][this.columns - 1] = this.vertical;
-        }
-        return matrix;
     }
 
-    one(){
-        let matrix = Utils.matrix(this.rows, this.columns);
-        for(let row = 1; row < this.rows - 1; row++){
+    addBot(matrix){
+        for(let column = 1 ; column < this.columns - 1 ; column++){
+            matrix[this.rows - 1][column] = this.horizontal;
+        }
+    }
+
+    addLeftTop(matrix){
+        for(let row = 1 ; row < Math.floor(this.rows / 2); row++){
+            matrix[row][0] = this.vertical;
+        }
+    }
+
+    addLeftBot(matrix){
+        for(let row = Math.floor(this.rows / 2) + 1 ; row < this.rows - 1; row++){
+            matrix[row][0] = this.vertical;
+        }
+    }
+
+    addRightTop(matrix){
+        for(let row = 1 ; row < Math.floor(this.rows / 2); row++){
             matrix[row][this.columns - 1] = this.vertical;
         }
-        if(this.size % 2 === 0){
-            matrix[Math.floor(this.rows / 2)][this.columns - 1] = null;
+    }
+
+    addRightBot(matrix){
+        for(let row = Math.floor(this.rows / 2) + 1 ; row < this.rows - 1; row++){
+            matrix[row][this.columns - 1] = this.vertical;
         }
-        return matrix;
     }
 
 }
